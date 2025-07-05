e5a99f35c71a70f41e1151e661ac4028e5d07c32
JuanCarlos3 <hwankim2k@gmail.com>
2025-07-05 14:49:15 -0700

    Added better comments cleaned up naming

--- concurrentImplementations.py
+++ concurrentImplementations.py
@@ -52,6 +52,10 @@ def threaded_sequential_compute_primes(numOfTasks, primeNumber):
     print("Total thread time: {:.2f} seconds".format(total_end_time - total_start_time))   
 
 
+# This function uses a thread pool to compute prime numbers concurrently.
+# It creates a fixed number of threads and submits tasks to the pool.
+# As there is a static number of threads, we save runtime as we do not have to create and join threads for each task.
+# This is useful when we may have multiple smaller tasks where creating the threads may take significant time.
 def thread_pool_compute_primes(numOfThreads, numOfTasks, primeNumber):
 
     total_start_time = time.perf_counter()
@@ -61,23 +65,31 @@ def thread_pool_compute_primes(numOfThreads, numOfTasks, primeNumber):
     total_end_time = time.perf_counter()
     print("Total thread time: {:.2f} seconds".format(total_end_time - total_start_time))   
 
+
+
 async def asyncio_computer_prime(threadNum):
     await worker(threadNum)
     # await asyncio.to_thread(worker, threadNum)
 
+
+#This function creates multiple asynchronous tasks to compute prime numbers concurrently.
+#It uses asyncio to run the tasks in an event loop.
 async def asyncio_compute_primes(numOfTasks, primeNumber):
     total_start_time = time.perf_counter()
-    futures = [asyncio.create_task(asyncio_computer_prime(_)) for _ in range(numOfTasks)]
-    await asyncio.gather(*futures)
+    tasks = [asyncio.create_task(asyncio_computer_prime(_)) for _ in range(numOfTasks)]
+    await asyncio.gather(*tasks)
 
     total_end_time = time.perf_counter()
     print("Total thread time: {:.2f} seconds".format(total_end_time - total_start_time))   
 
 
+
+
+
 # threaded_compute_primes(2, primeNumber)
 # threaded_sequential_compute_primes(2, primeNumber)
 # thread_pool_compute_primes(2, 2, primeNumber)
-asyncio.run(asyncio_compute_primes(2, primeNumber))
+# asyncio.run(asyncio_compute_primes(2, primeNumber))
 
 
 
